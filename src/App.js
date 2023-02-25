28ae814b71cb300ce65637bc4156f43780706835
felipeffranco <felipeffranco@outlook.com>
2023-02-25 14:15:41 -0300

    navbar simple

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage.js'
 import SignUpPage from './pages/SignUpPage.js'
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className='App'>
       <Routes>
+        <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/sign-up' element={<SignUpPage />} />
       </Routes>
