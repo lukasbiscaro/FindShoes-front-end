1375856a2eba9d4a87ffe582902f058deb324707
Lukas de Oliveira Biscaro <lukas.biscaro@hotmail.com>
2023-03-18 02:57:47 -0300

    Solved problema about comment author home page.

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -3,38 +3,35 @@ import axios from 'axios'
 
 const CommentCard = ({ comment }) => {
 
-    const [dataProfiles, setDataProfiles] = useState([])
+    const [authorName, setAuthorName] = useState('')
 
     useEffect(() => {
-        axios.get(`${process.env.REACT_APP_API_URL}/all-profiles`)
-            .then(response => {
-                setDataProfiles(response.data)
+        axios.get(`${process.env.REACT_APP_API_URL}/users/${comment.userId}`)
+            .then((response) => {
+                const author = response.data
+                setAuthorName(`${author.firstName} ${author.lastName}`)
             })
-            .catch(err => console.log(err))
-    }, [])
+            .catch((err) => console.log(err))
+    }, [comment.userId])
+
 
     return (
         <div className="group relative p-4 bg-white rounded-lg">
-            <div className="flex justify-between relative z-50">
-                <div>
-                    <p className="text-sm text-gray-900 mb-4">
-                        <span className="text-gray-900 text-xs font-regular max-w-xs truncate leading-5 whitespace-normal line-clamp-4">
-                            {comment.text}
-                        </span>
-                    </p>
-                    {
-                        dataProfiles.map((profile) => {
-                            return (
-                                <p className='text-black text-md font-bold capitalize'>
-                                    {profile.user.firstName} {profile.user.lastName}
-                                </p>
-                            )
-                        })
-                    }
-                    <p className='text-highlightPrimary text-sm'>
-                        CEO
-                    </p>
-                </div>
+            <div className="flex flex-col justify-between relative z-50">
+                <p className="text-sm text-gray-900 mb-4">
+                    <span className="text-gray-900 text-xs font-regular max-w-xs truncate leading-5 whitespace-normal line-clamp-4">
+                        {comment.text}
+                    </span>
+                </p>
+                {
+                    authorName && (
+                        <p className='text-black text-md font-bold capitalize'>
+                            {authorName}
+                        </p>
+                    )}
+                <p className='text-highlightPrimary text-sm'>
+                    CEO
+                </p>
             </div>
         </div>
     )
